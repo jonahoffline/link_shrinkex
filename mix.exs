0a7e74a083ce31423a70cb1b235f3c28e5740f99
Jonah Ruiz <jonah@pixelhipsters.com>
2013-10-24 00:14:32 -0400

    Update JSex dependency to 0.2 and update LinkShrinkex version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LinkShrinkex.Mixfile do
 
   def project do
     [ app: :link_shrinkex,
-      version: "0.0.2",
+      version: "0.0.3",
       deps: deps ]
   end
 
@@ -15,7 +15,7 @@ defmodule LinkShrinkex.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [{ :jsex, "0.0.1", github: "talentdeficit/jsex" }]
+    [{ :jsex, "0.2", github: "talentdeficit/jsex" }]
   end
 end
 
