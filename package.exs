b9ef66ddd06bebb8e2a44298508a6d7d2d54d6e6
Jonah Ruiz <jonah@pixelhipsters.com>
2014-01-26 18:40:49 -0400

    Update expm homepage, license, platforms and version

--- package.exs
+++ package.exs
@@ -1,6 +1,11 @@
-Expm.Package.new(name: "link_shrinkex", description: "Create short URLs using Google's URL Shortener API.",
-                 version: "0.0.4", keywords: ["Google", "ShortenerAPI", "urls", "shrinkUrls"],
-                 maintainers: [[name: "Jonah Ruiz",
-                                email: "jonahoffline@pixelhipsters.com"]],
-                 repositories: [[github: "jonahoffline/link_shrinkex", tag: "v0.0.4"]])
+version = String.strip(File.read!("VERSION"))
 
+Expm.Package.new(name: "link_shrinkex",
+  description: "Create short URLs using Google's URL Shortener API.",
+  version: version,
+  keywords: ["Google", "ShortenerAPI", "urls", "shrinkUrls"],
+  homepage: "http://www.github.com/jonahoffline/link_shrinkex",
+  licenses: ["MIT"],
+  platforms: ["Elixir-0.12.2"],
+  maintainers: [[name: "Jonah Ruiz", email: "jonahoffline@pixelhipsters.com"]],
+  repositories: [[github: "jonahoffline/link_shrinkex", tag: "v#{version}"]])
