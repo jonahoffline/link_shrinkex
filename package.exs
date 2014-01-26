14539730a2171b70036c9abde6cdca9547471e2f
Jonah Ruiz <jonah@pixelhipsters.com>
2014-01-26 04:45:19 -0400

    Add package conf to publish library to expm: Elixir Package Manager

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "link_shrinkex", description: "Create short URLs using Google's URL Shortener API.",
+                 version: "0.0.4", keywords: ["Google", "ShortenerAPI", "urls", "shrinkUrls"],
+                 maintainers: [[name: "Jonah Ruiz",
+                                email: "jonahoffline@pixelhipsters.com"]],
+                 repositories: [[github: "jonahoffline/link_shrinkex", tag: "v0.0.4"]])
+
