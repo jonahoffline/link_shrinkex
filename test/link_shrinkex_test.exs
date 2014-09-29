5c61728d1c216d8562be13f63ada4430e6241c73
Jonah Ruiz <jonah@pixelhipsters.com>
2014-09-28 20:34:21 -0400

    Fix breaking tests due to elixir 1.0 release

--- test/link_shrinkex_test.exs
+++ test/link_shrinkex_test.exs
@@ -26,21 +26,21 @@ defmodule LinkShrinkexTest do
   test "returns short and long urls with [:urls] option" do
     LinkShrinkex.start
 
-    expected_response_urls = { :ok, [id: "http://goo.gl/Shz0u", longUrl: "http://www.elixir-lang.org/"] }
+    expected_response_urls = { :ok, %{id: "http://goo.gl/Shz0u", longUrl: "http://www.elixir-lang.org/"} }
     assert LinkShrinkex.shrink_url("http://www.elixir-lang.org", [:urls]) == expected_response_urls
   end
 
   test "returns parsed JSON with [:json] option" do
     LinkShrinkex.start
 
-    expected_response = {:ok,"{\"kind\":\"urlshortener#url\",\"id\":\"http://goo.gl/Shz0u\",\"longUrl\":\"http://www.elixir-lang.org/\"}"}
+    expected_response = {:ok,"{\"id\":\"http://goo.gl/Shz0u\",\"kind\":\"urlshortener#url\",\"longUrl\":\"http://www.elixir-lang.org/\"}"}
     assert LinkShrinkex.shrink_url("http://www.elixir-lang.org", [:json]) == expected_response
   end
 
   test "returns list with [:list] option" do
     LinkShrinkex.start
 
-    expected_response = {:ok,[kind: "urlshortener#url", id: "http://goo.gl/Shz0u", longUrl: "http://www.elixir-lang.org/"]}
+    expected_response = {:ok,%{id: "http://goo.gl/Shz0u", kind: "urlshortener#url", longUrl: "http://www.elixir-lang.org/"}}
     assert LinkShrinkex.shrink_url("http://www.elixir-lang.org", [:list]) == expected_response
   end
 
@@ -59,47 +59,47 @@ defmodule LinkShrinkexTest do
   end
 
   test "shrink_url raises Error for invalid argument" do
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.shrink_url(1234567890)
     end
 
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.shrink_url(666.666)
     end
 
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.shrink_url({})
     end
 
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.shrink_url(:atom)
     end
   end
 
   test "prepare_request_body raises Error for invalid argument" do
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.prepare_request_body(1234567890)
     end
 
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.prepare_request_body(666.666)
     end
 
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.prepare_request_body({})
     end
 
-    assert_raise LinkShrinkex.Error, fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.prepare_request_body(:atom)
     end
   end
 
   test "raises error with message for invalid arguments" do
-    assert_raise LinkShrinkex.Error, "nil cannot be sent to Google Url Shortner API.", fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.prepare_request_body({})
     end
 
-    assert_raise LinkShrinkex.Error, "nil cannot be sent to Google Url Shortner API.", fn ->
+    assert_raise ArgumentError, fn ->
       LinkShrinkex.shrink_url({})
     end
   end
